7e6dbf5287b801c9f5776d5dd3b07133b6c69e5d
bala-amypo <bala@amypo.in>
2025-12-18 13:01:43 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,45 +1,11 @@
-package com.example.demo.security;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-
 @Component
 public class JwtTokenProvider {
 
-    private final String SECRET_KEY = "secret-key";
-    private final long EXPIRATION = 86400000; // 1 day
+    private final String secret;
+    private final long validityInMs;
 
-    public String generateToken(String email) {
-
-        return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            getClaims(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public String getEmailFromToken(String token) {
-        return getClaims(token).getSubject();
-    }
-
-    private Claims getClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody();
+    public JwtTokenProvider(String secret, long validityInMs) {
+        this.secret = secret;
+        this.validityInMs = validityInMs;
     }
 }
